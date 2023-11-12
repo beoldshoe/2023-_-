be26c6ecdf25f7dff2e54c5e14ab00fec16440bf
youngin kim <hardworkingini@gmail.com>
2023-11-12 23:10:01 +0900

    로그인 기능 완성

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -1,19 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Settings extends StatelessWidget {
   const Settings({super.key});
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
+      appBar: AppBar(
+        title: Text('설정 페이지'),
+      ),
       body: Center(
-        child: SizedBox(
-          height: 500,
-          width: 250,
-          child: Lottie.asset("lottie/settings.json"),
+        child: ElevatedButton(
+        child: Text('Google Account Login'),
+    onPressed: () {
+      signInWithGoogle();
+    }
         ),
+
       ),
     );
   }
+  Future<UserCredential> signInWithGoogle() async {
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+    final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+    UserCredential userCredential = await FirebaseAuth.instance.signInWithCredential(credential);
+
+    // 사용자 정보를 Firestore에 저장
+    CollectionReference usersCollection = FirebaseFirestore.instance.collection('users');
+    await usersCollection.doc(userCredential.user!.uid).set({
+      'email': userCredential.user!.email,
+      'name': userCredential.user!.displayName,
+    });
+
+    return userCredential;
+  }
 }
+
